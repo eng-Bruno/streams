4b5921e39cae7d8635348007f8cb717b61a17488
Bruno Oliveira <brunooliveira.unb@gmail.com>
2022-04-01 13:23:47 -0300

    como nao utilizar navegaçao com react router

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,21 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 const PageOne = () => {
-  return <div>PageOne</div>;
+  return (
+    <div>
+      PageOne
+      <a href="/pagetwo"> Navigate to Page Two</a>
+    </div>
+  );
 };
 
 const PageTwo = () => {
-  return <div>PageTwo</div>;
+  return (
+    <div>
+      PageTwo
+      <a href="/"> Navigate to Page One</a>
+    </div>
+  );
 };
 
 const App = () => {
