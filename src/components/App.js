3445e0bce0368101ae92a109f3944c1b08fd2012
Bruno Oliveira <brunooliveira.unb@gmail.com>
2022-04-07 22:28:29 -0300

    Navigating with React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const PageOne = () => {
   return (
     <div>
       PageOne
-      <a href="/pagetwo"> Navigate to Page Two</a>
+      <Link to="/pagetwo"> Navigate to Page Two</Link>
     </div>
   );
 };
@@ -14,7 +14,7 @@ const PageTwo = () => {
   return (
     <div>
       PageTwo
-      <a href="/"> Navigate to Page One</a>
+      <Link to="/"> Navigate to Page One</Link>
     </div>
   );
 };
