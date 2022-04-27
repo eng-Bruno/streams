a0591fb97a17da80f2cfea7c127e67cf587ddaf5
Bruno Oliveira <brunooliveira.unb@gmail.com>
2022-04-27 11:38:51 -0300

    renderizando status de autenticação

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,18 +1,43 @@
+import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 import React from "react";
 
 class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "437647129407-n0gqr675bt2306tp2ep2u10h4kcghu3n.apps.googleusercontent.com",
-        scope: "email",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "437647129407-n0gqr675bt2306tp2ep2u10h4kcghu3n.apps.googleusercontent.com",
+          scope: "email",
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
     });
   }
+
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I dont know if we are signed in</div>;
+    } else if (this.state.isSignedIn === true) {
+      return <div>I am signed in!</div>;
+    } else {
+      return <div>I am not signed in!</div>;
+    }
+  }
+
   render() {
-    return <div>Google Auth</div>;
+    return <div>{this.renderAuthButton()}</div>;
   }
 }
 
 export default GoogleAuth;
+
+/* AUTH COMPONENT:
+ * -> Get a reference to the 'auth' object after it is initialized
+ * -> Figure out if the user is currently signed in
+ * -> Print their authentication status on the screen
+ */
