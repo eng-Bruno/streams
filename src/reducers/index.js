1274f562f6c649978fbbab41ef4f84cdd455154d
Bruno Oliveira <brunooliveira.unb@gmail.com>
2022-05-04 11:43:04 -0300

    conectando redux form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
+import { reducer as formReducer } from "redux-form"; //trocando o nome da variavel para formReducer
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
