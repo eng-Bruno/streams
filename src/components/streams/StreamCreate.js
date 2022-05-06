7e0dd1466ec0b7fdd1b588f79ecfb58ef0f3d38f
Bruno Oliveira <brunooliveira.unb@gmail.com>
2022-05-06 11:16:22 -0300

    validaçao do form input

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { updateSyncErrors } from "redux-form";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
@@ -32,6 +33,20 @@ class StreamCreate extends React.Component {
     );
   }
 }
+
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    // only ran if the user did not enter a title
+    errors.title = "You must enter a title";
+  }
+
+  if (!formValues.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
 
 export default reduxForm({
   form: "streamCreate",
