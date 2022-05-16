84db40ade696a5211aa7ef71953724a142b99ea5
Bruno Oliveira <brunooliveira.unb@gmail.com>
2022-05-16 09:43:16 -0300

    merging lists of records

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as formReducer } from "redux-form"; //trocando o nome da variavel para formReducer
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
   auth: authReducer,
   form: formReducer,
+  streams: streamReducer,
 });
--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -5,10 +5,13 @@ import {
   CREATE_STREAM,
   EDIT_STREAM,
   DELETE_STREAM,
-} from "../action/types";
+} from "../actions/types";
 
 export default (state = {}, action) => {
   switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
 
